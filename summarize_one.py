3b3b4cd002516b85b698500c2d554c581a2283c4
Thomas Brettin <brettin@anl.gov>
2024-02-13 12:14:47 -0600

    added prompt

--- summarize_one.py
+++ summarize_one.py
@@ -14,3 +14,23 @@ llm = ARGO_LLM(argo=ArgoWrapper())
 
 print(llm("hello world"))
 
+prompt="""You are an advanced AGI for science. Please read the following
+paper and answer the following questions.
+
+1. What is the paper about?
+2. What is the central question or questions that the paper aims to answer?
+3. What are the key findings that relate to the central question or questions?
+4. If there are unanswered questions in the paper, what are those?
+
+Please provide the output with the following headings:
+
+SUMMARY
+CENTRAL QUESTION(S)
+FINDINGS
+OPEN QUESTIONSi
+
+If you are unsure of the answer to any of the questions, please say you are unsure.
+
+Here is the paper """ + paper
+
+
